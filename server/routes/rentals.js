2cd0cd1e8369d74c3efd08dbe0c5c65a8e0a91dc
Andrei <andrejborozdin@yandex.ru>
2019-01-25 12:01:36 +0300

    Add endpoint for creation and search rental

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -2,31 +2,55 @@ const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
 const UserCtrl = require('../controllers/user');
+const User = require('../models/user');
+const MongooseHelpers = require('../helpers/mongoose');
 
 router.get('/secret', UserCtrl.authMiddleware, function (req, res) {
   res.json({'secret': true});
-})
-
-router.get('', function (req, res) {
-  Rental.find({})
-        .select('-bookings')
-        .exec(function (err, foundRentals) {
-          res.json(foundRentals);
-        });
 });
 router.get('/:id', function (req, res) {
   const rentalId = req.params.id
   Rental.findById(rentalId)
-        .populate('user', 'username-_id')
-        .populate('bookings', 'startAt, endAt-_id')
-        .exec(function (err, foundRental) {
-          if(err){
-           return res.status(422).send({
-            errors: [{title: 'Rental Error', default: 'Could not find rental'}]
-            })
-          }
-        return res.json(foundRental);
+    .populate('user', 'username-_id')
+    .populate('bookings', 'startAt, endAt-_id')
+    .exec(function (err, foundRental) {
+      if(err){
+        return res.status(422).send({
+          errors: [{title: 'Rental Error', default: 'Could not find rental'}]
         })
+      }
+      return res.json(foundRental);
+    })
 });
+router.post('', UserCtrl.authMiddleware, function (req, res) {
+ const {title, city, street, category, image, description, dailyRate} = req.body;
+ const user = res.locals.user;
+ const rental = new Rental({title, city, street, category, image, description, dailyRate});
+ rental.user = user;
+ Rental.create(rental, function (err, newRental) {
+   if(err){
+     return res.status(422).send({errors: MongooseHelpers.normaliseErrors(err.errors)});
+   }
+   User.update({_id: user.id}, {$push: {rentals: newRental}}, function () {});
+   return res.json(newRental)
+ });
+});
+
+router.get('', function (req, res) {
+  const city = req.query.city;
+  const query = city ? {city: city.toLowerCase()} : {}
+       Rental.find(query)
+      .select('-bookings')
+      .exec(function (err, foundRentals) {
+        if(err){
+          return res.status(422).send({errors: MongooseHelpers.normaliseErrors(err.errors)});
+        }
+        if(city && foundRentals.length === 0){
+          return res.status(422).send({
+            errors: [{title: 'No rental found', default: `There no rentals for city ${city}`}]})
+        }
+        return res.json(foundRentals);
+      });
+  });
 
 module.exports = router
